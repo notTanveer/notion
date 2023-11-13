4d6f30e0bd67af373b6ae8c2d6d791d53c4524bc
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-11-13 23:27:47 +0530

    Initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type {Config} from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+
+if (!process.env.DATABASE_URL){
+    console.log('DATABASE_URL not found in .env file')
+}
+
+export default {
+    schema: '.src/lib/supabase/schema.ts',
+    out: './migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL || '',
+    },
+} satisfies Config;
---
+++ migrations/schema.ts
@@ -0,0 +1,1 @@
+import drizzleConfig from "../drizzle.config";
