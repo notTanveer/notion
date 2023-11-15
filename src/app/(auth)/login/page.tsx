9cc00a62f701465ce01e2f0d530cb90f8645d361
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-11-16 01:26:45 +0530

    sign up

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FormSchema } from '@/lib/types';
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchema } from "@/lib/types";
 import {
   Form,
   FormControl,
@@ -12,24 +12,24 @@ import {
   FormField,
   FormItem,
   FormMessage,
-} from '@/components/ui/form';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from '../../../../public/cypresslogo.svg';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import Loader from '@/components/global/Loader';
-import { Separator } from '@/components/ui/separator';
-import { actionLoginUser } from '@/lib/server-action/auth-action';
+} from "@/components/ui/form";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../../public/cypresslogo.svg";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/global/Loader";
+import { Separator } from "@/components/ui/separator";
+import { actionLoginUser } from "@/lib/server-action/auth-action";
 
 const LoginPage = () => {
   const router = useRouter();
-  const [submitError, setSubmitError] = useState('');
+  const [submitError, setSubmitError] = useState("");
 
   const form = useForm<z.infer<typeof FormSchema>>({
-    mode: 'onChange',
+    mode: "onChange",
     resolver: zodResolver(FormSchema),
-    defaultValues: { email: '', password: '' },
+    defaultValues: { email: "", password: "" },
   });
 
   const isLoading = form.formState.isSubmitting;
@@ -42,14 +42,14 @@ const LoginPage = () => {
       form.reset();
       setSubmitError(error.message);
     }
-    router.replace('/dashboard');
+    router.replace("/dashboard");
   };
 
   return (
     <Form {...form}>
       <form
         onChange={() => {
-          if (submitError) setSubmitError('');
+          if (submitError) setSubmitError("");
         }}
         onSubmit={form.handleSubmit(onSubmit)}
         className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
@@ -62,12 +62,7 @@ const LoginPage = () => {
           justify-left
           items-center"
         >
-          <Image
-            src={Logo}
-            alt="cypress Logo"
-            width={50}
-            height={50}
-          />
+          <Image src={Logo} alt="cypress Logo" width={50} height={50} />
           <span
             className="font-semibold
           dark:text-white text-4xl first-letter:ml-2"
@@ -88,11 +83,7 @@ const LoginPage = () => {
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input
-                  type="email"
-                  placeholder="Email"
-                  {...field}
-                />
+                <Input type="email" placeholder="Email" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -105,11 +96,7 @@ const LoginPage = () => {
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  {...field}
-                />
+                <Input type="password" placeholder="Password" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -122,14 +109,11 @@ const LoginPage = () => {
           size="lg"
           disabled={isLoading}
         >
-          {!isLoading ? 'Login' : <Loader />}
+          {!isLoading ? "Login" : <Loader />}
         </Button>
         <span className="self-container">
-          Dont have an account?{' '}
-          <Link
-            href="/signup"
-            className="text-primary"
-          >
+          Dont have an account?{" "}
+          <Link href="/signup" className="text-primary">
             Sign Up
           </Link>
         </span>
